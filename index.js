42f6cea63cc194821901556ca2522ac78f0d7a94
XavierMarcotte <xaviermarcotte06@gmail.com>
2023-04-04 21:01:20 +0200

    projet

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 const inputElement = document.querySelector('input');
+const form = document.querySelector("#form");
+const todo = document.querySelector('.todo')
 
-function recuperer() {
-    inputElement.addEventListener('keyup', function(event) {
-      const inputValue = event.target.value; 
-      console.log(inputValue); 
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+});
+
+function ajouter() {
+  const nouveauxTitres = document.querySelectorAll('.todo h2');
+  nouveauxTitres.forEach(titre => {
+    titre.addEventListener('click', () => {
+      titre.classList.toggle('checked');
     });
+  });
 }
+
+inputElement.addEventListener('keydown', function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault;
+    const inputValue = event.target.value; 
+    const note = document.createElement('h2');
+    note.textContent = inputValue;
+    todo.appendChild(note)
+    ajouter();
+    event.target.value ='';
+  }
+});
+
+
+
+
+
